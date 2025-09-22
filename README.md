f8b2450ca97592727bcabfc783cdacd7a9b270a2
Fridolin Zschocke <fridolin.zschocke@gmx.de>
2025-09-22 12:42:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MinecraftItemPriceCalculator
-Calculates the price of minecraft items using recipes
+I wrote this to calculate the price of every item for my minecraft server. If you just need the prices you can copy prices.json.
+
+# How it works
+The prices of items are calculated using manually set prices for all uncraftable items (can be found in base\_prices.json)
+I calculated the prices based on how many items you could farm with end game loot in one hour. One hour of farming should be worth 100000$.
+
+e.g:
+To calculate the price of dirt, I mined as much dirt es I could with a effi V netherite shovel in one minute (10 stacks) -> in one hour (600 stacks) -> 38400 should be worth 100000$ -> one dirt is worth 2.6$
+
+The script then goes through all craftable items and looks for the cheapest way to craft it (this includes methods like smelting without price increase).
+
+# Run it yourself
+Before you can run the script you will have to replace the minecraft folder with the actual minecraft folder.
---
+++ minecraft_data.py
@@ -0,0 +1,69 @@
+import json
+import os
+import sys
+
+recipe_folder = "minecraft/data/minecraft/recipe/"
+
+if not os.path.isdir(recipe_folder):
+    print("recipe folder not found!")
+    print("Have you imported the minecraft folder as instructed in the README?")
+    sys.exit("")
+
+recipe_files = [f for f in os.listdir(recipe_folder) if os.path.isfile(recipe_folder + f)]
+
+recipes = []
+for recipe_file in recipe_files:
+    recipes.append(json.loads(open(recipe_folder + recipe_file).read()))
+
+craftable_items = []
+
+for recipe in recipes:
+    if not "result" in recipe:
+        continue
+
+    result = recipe["result"]    
+
+    if not ("id" in result):
+        continue
+
+    item = recipe["result"]["id"]
+    if item in craftable_items:
+        continue
+    craftable_items.append(item)
+print("found " + str(len(craftable_items)) + " craftable_items")
+
+def get_ingredients(recipe):
+    ingredients = []
+    
+    def add_ingredient(ingredient):
+        if "item" in ingredient:
+            ingredients.append(ingredient["item"])
+        elif "tag" in ingredient:
+            ingredients.append("tag:" + ingredient["tag"])
+        else:
+            ingredients.append(ingredient[0]["item"])
+
+    if "ingredient" in recipe:
+        add_ingredient(recipe["ingredient"])
+    elif "ingredients" in recipe:
+        for ingredient in recipe["ingredients"]:
+            add_ingredient(ingredient)
+    elif "key" in recipe:
+        for row in recipe["pattern"]:
+            for itemKey in row:
+                if itemKey == ' ':
+                    continue
+                add_ingredient(recipe["key"][itemKey])
+    
+    return ingredients
+
+def get_uncraftable_items():
+    uncraftable_items = {}
+    for recipe in recipes:
+        for ingredient in get_ingredients(recipe):
+            if ingredient not in uncraftable_items and ingredient not in craftable_items:
+                uncraftable_items[ingredient] = 1
+    return uncraftable_items
+
+if len(sys.argv) > 1 and sys.argv[1] == 'uncraftable':
+    print(get_uncraftable_items())
